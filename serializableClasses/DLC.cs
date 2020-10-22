ee82f84a9cb192db1db991a7fa38ab6a9485eea9
montag.lennard@gmail.com <montag.lennard@gmail.com>
2020-10-22 16:09:29 +0200

    final commit before publish

---
+++ serializableClasses/DLC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modinfo
+{
+    class DLC
+    {
+        //use: "Anarchist", "SunkenTreasures", "Botanica", "ThePassage", "SeatOfPower", "BrightHarvest", "LandOfLions", "Christmas", "AmusementPark, "CityLife"
+        public String DLC { get; set; }
+        //use: "required", "partly", "atLeastOneRequired"
+        public String Dependant { get; set; }
+    }
+}
