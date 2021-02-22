0ce65490f68a409809a23a5cedf9fbf1d692e3ff
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2021-02-22 23:17:41 +0100

    Update DLC.cs

--- serializableClasses/DLC.cs
+++ serializableClasses/DLC.cs
@@ -7,7 +7,7 @@ namespace SerializableModinfo
     class Dlc
     {
         public Dlc() { }
-        //use: "Anarchist", "SunkenTreasures", "Botanica", "ThePassage", "SeatOfPower", "BrightHarvest", "LandOfLions", "Christmas", "AmusementPark, "CityLife"
+        //use: "Anarchist", "SunkenTreasures", "Botanica", "ThePassage", "SeatOfPower", "BrightHarvest", "LandOfLions", "Christmas", "AmusementPark, "CityLife", "Docklands", "Tourism", "Highlife"
         public String DLC { get; set; }
         //use: "required", "partly", "atLeastOneRequired"
         public String Dependant { get; set; }
