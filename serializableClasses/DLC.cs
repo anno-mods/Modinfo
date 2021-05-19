23ac3ca99ef8647e98ea1cd1a401de353247d835
montag.lennard@gmail.com <montag.lennard@gmail.com>
2021-05-19 23:30:22 +0200

    Add IDs for S3 Cosmetic DLCs

--- serializableClasses/DLC.cs
+++ serializableClasses/DLC.cs
@@ -7,7 +7,7 @@ namespace SerializableModinfo
     class Dlc
     {
         public Dlc() { }
-        //use: "Anarchist", "SunkenTreasures", "Botanica", "ThePassage", "SeatOfPower", "BrightHarvest", "LandOfLions", "Christmas", "AmusementPark, "CityLife", "Docklands", "Tourism", "Highlife"
+        //use: "Anarchist", "SunkenTreasures", "Botanica", "ThePassage", "SeatOfPower", "BrightHarvest", "LandOfLions", "Christmas", "AmusementPark, "CityLife", "Docklands", "Tourism", "Highlife", "VehicleSkins", "VibrantCity", "PedestrianZone"
         public String DLC { get; set; }
         //use: "required", "partly", "atLeastOneRequired"
         public String Dependant { get; set; }
